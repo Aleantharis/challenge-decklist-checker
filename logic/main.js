78565a58e8b278e2e7819e13cd321ac7261c833f
Aleantharis <alex.steinke@edumail.at>
2022-12-04 20:35:18 +0100

    Update main.js

--- logic/main.js
+++ logic/main.js
@@ -1,6 +1,6 @@
-import * as bl from '../data/banlist.json';
-import * as sl from '../data/setlist.json';
-import * as db from '../data/card-db-essential.json';
+import bl from '../data/banlist.json' assert { type: "json" };
+import sl from '../data/setlist.json' assert { type: "json" };
+import db from '../data/card-db-essential.json' assert { type: "json" };
 
 
 $(function() {
