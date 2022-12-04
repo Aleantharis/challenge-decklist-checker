ec527a3042f29a51138df357197395ad2ba29389
Alex <alex.steinke@edumail.at>
2022-12-04 20:51:47 +0100

    Fixed non-array-bug. Compressed card-db.

--- logic/main.js
+++ logic/main.js
@@ -3,35 +3,44 @@ import sl from '../data/setlist.json' assert { type: "json" };
 import db from '../data/card-db-essential.json' assert { type: "json" };
 
 
-$(function() {
+$(function () {
   const cardDb = db;
   const bans = bl;
   const setList = sl;
 
- $("#in").attr("placeholder", "Please paste deck here (in MTG online's deck format (*.dek))...\n\nExample:\n1 Library of Congress\n1 Cryptic Gateway\n1 Azami, Lady of Scrolls")
+  $("#in").attr("placeholder", "Please paste deck here (in MTG online's deck format (*.dek))...\n\nExample:\n1 Library of Congress\n1 Cryptic Gateway\n1 Azami, Lady of Scrolls")
 
-function validateCard(cardName) {
-  return (JSON.parse(cardDb)[cardName].filter(value => setList.includes(value))).length > 0;
-}
+  function validateCard(cardName) {
+    if (Array.isArray(cardDb[cardName])) {
+      return (cardDb[cardName].filter(value => setList.includes(value))).length > 0;
+    }
+    else if (cardDb[cardName] !== undefined) {
+      return setList.includes(cardDb[cardName]);
+    }
+    else {
+      console.log("git gud");
+      return true;
+    }
+  }
 
-function validateDeck() {
+  function validateDeck() {
     const deck = $("#in").val().split('\n');
 
     deck.forEach((element, index) => {
-    	var san = element;
+      var san = element;
       san = (san.replace(/^\d ()?/, "")).trim();
       deck[index] = san;
     });
 
     var bannedCards = [];
     var unavailCards = [];
-    deck.forEach(function(value) {
+    deck.forEach(function (value) {
       var idx = bans.indexOf(value);
       if (idx >= 0) {
         bannedCards.push(value);
       }
 
-      if(!validateCard(value)) {
+      if (!validateCard(value)) {
         unavailCards.push(value);
       }
     });
@@ -39,7 +48,7 @@ function validateDeck() {
     let banList = [...new Set(bannedCards)];
     var banTxt = "";
     if (banList.length > 0) {
-      banList.forEach(function(value) {
+      banList.forEach(function (value) {
         banTxt += "<li>" + value + "</li>";
       });
     }
@@ -47,7 +56,7 @@ function validateDeck() {
     let errList = [...new Set(unavailCards)];
     var errTxt = "";
     if (errList.length > 0) {
-      errList.forEach(function(value) {
+      errList.forEach(function (value) {
         errTxt += "<li>" + value + "</li>";
       });
     }
@@ -57,5 +66,5 @@ function validateDeck() {
   }
 
   $("#in").on('change keyup', validateDeck);
-  $("#in").bind('paste',function() {setTimeout(validateDeck);});
+  $("#in").bind('paste', function () { setTimeout(validateDeck); });
 });
